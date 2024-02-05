f766af7de7e46aea504772c4d18ba4a42b0d5e97
nithinp16 <npadigel@uncc.edu>
2024-02-05 07:05:16 +0530

    Added server, Axios, and ChartJS

--- server.js
+++ server.js
@@ -21,9 +21,25 @@ const budget = {
             title: 'Grocery',
             budget: 110
         },
+        {
+            title: 'Medical Expenses',
+            budget: 50
+        },
+        {
+            title: 'Car Insurance',
+            budget: 30
+        },
+        {
+            title: 'Shopping',
+            budget: 100
+        },
+        {
+            title: 'Health Insurance',
+            budget: 25
+        },
     ]
 };
-
+app.use('/',express.static('public'));
 
 app.get('/budget', (req, res) => {
     res.json(budget);
